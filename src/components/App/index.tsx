4f4e30e434f79ebd1678b8c864ff2397649a96f3
Chinedu Nweke <nwekealexchinedu@gmail.com>
2020-09-20 14:48:42 +0100

    created the handle submit function

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,22 +1,26 @@
 import React from "react";
 import "./App.css";
-import { Formik, Form } from "Formik";
+import { Formik, Form } from "formik";
 
-interface formValues {
+interface FormValues {
   name: string;
   position: string;
 }
 
-const initialValues: formValues = {
+const initialValues: FormValues = {
   name: "",
   position: "",
 };
 
 const App: React.FC = () => {
+  const handleSubmit = (values: FormValues): void => {
+    alert(JSON.stringify(values));
+  };
+
   return (
     <div className='App'>
       <h1>Sign Up</h1>
-      <Formik initialValues={initialValues}></Formik>
+      <Formik initialValues={initialValues} onSubmit={handleSubmit}></Formik>
     </div>
   );
 };

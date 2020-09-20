7b9d67b57dd0bfd8acbc629402810456c1665ce7
Chinedu Nweke <nwekealexchinedu@gmail.com>
2020-09-20 14:43:54 +0100

    created the form values interface

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,8 +1,24 @@
 import React from "react";
 import "./App.css";
+import { Formik, Form } from "Formik";
+
+interface formValues {
+  name: string;
+  position: string;
+}
+
+const initialValues: formValues = {
+  name: "",
+  position: "",
+};
 
 const App: React.FC = () => {
-  return <div className='App'>Hello</div>;
+  return (
+    <div className='App'>
+      <h1>Sign Up</h1>
+      <Formik initialValues={initialValues}></Formik>
+    </div>
+  );
 };
 
 export default App;
